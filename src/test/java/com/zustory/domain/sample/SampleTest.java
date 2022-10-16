9998f912139aad09e953a7fe2925b03ed812658a
ypd01018 <ypd01018@naver.com>
2022-10-16 21:56:27 +0900

    feature: sampleRepository unit test 추가

---
+++ src/test/java/com/zustory/domain/sample/SampleTest.java
@@ -0,0 +1,43 @@
+package com.zustory.domain.sample;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SampleTest {
+
+    @Autowired
+    SampleRepository sampleRepository;
+
+    @AfterEach
+    void cleanUp() {
+        sampleRepository.deleteAll();
+    }
+
+    @Test
+    void 샘플_저장_조회() {
+        // given
+        String title = "sample 제목";
+        String content = "sample 내용";
+
+        sampleRepository.save(Sample.builder().title(title).content(content).build());
+
+        //when
+        List<Sample> sampleList = sampleRepository.findAll();
+
+        //then
+        Sample sample = sampleList.get(0);
+        assertThat(sample.getTitle()).isEqualTo(title);
+        assertThat(sample.getContent()).isEqualTo(content);
+    }
+
+}
