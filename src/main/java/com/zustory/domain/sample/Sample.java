f748926358a143f6928adc4dd4e9973ec87494d7
ypd01018 <ypd01018@naver.com>
2022-10-16 23:03:16 +0900

    feature: sample Entity 수정

--- src/main/java/com/zustory/domain/sample/Sample.java
+++ src/main/java/com/zustory/domain/sample/Sample.java
@@ -1,5 +1,6 @@
 package com.zustory.domain.sample;
 
+import com.zustory.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Sample {
+public class Sample extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
