104e6224db6bd9a0945f13e133930bf95f6b2651
ypd01018 <ypd01018@naver.com>
2022-11-11 21:33:04 +0900

    test: 주류, 카테고리 테스트 데이터 삽입

---
+++ src/main/resources/db/migration/common/V1_0_001__insert_categoty.sql
@@ -0,0 +1,9 @@
+INSERT INTO category
+    (type, created_at, modified_at)
+VALUES
+      ('WINE', NOW(), NOW())
+     ,('BEER', NOW(), NOW())
+     ,('SOJU', NOW(), NOW())
+     ,('KOREAN_TRADITIONAL_ALCOHOL', NOW(), NOW())
+     ,('LIQUOR', NOW(), NOW())
+     ,('ETC', NOW(), NOW());
---
+++ src/main/resources/db/migration/test/V1_0_900_002__insert_beverage.sql
@@ -0,0 +1,9 @@
+INSERT INTO beverage
+    (beverage_name, company, abv, contents, image_url, category_id, created_at, modified_at)
+VALUES
+    ('참나무통 맑은 이슬', '하이트 진로', '16.0', '하이트 진로에서 발매한 참나무통 맑은 이슬', 'image.test.url', 3, NOW(), NOW()),
+    ('참나무통 맑은 이슬2', '하이트 진로', '16.0', '하이트 진로에서 발매한 참나무통 맑은 이슬', 'image.test.url', 3, NOW(), NOW()),
+    ('참나무통 맑은 이슬3', '하이트 진로', '16.0', '하이트 진로에서 발매한 참나무통 맑은 이슬', 'image.test.url', 3, NOW(), NOW()),
+    ('참나무통 맑은 이슬4', '하이트 진로', '16.0', '하이트 진로에서 발매한 참나무통 맑은 이슬', 'image.test.url', 3, NOW(), NOW()),
+    ('참나무통 맑은 이슬5', '하이트 진로', '16.0', '하이트 진로에서 발매한 참나무통 맑은 이슬', 'image.test.url', 3, NOW(), NOW()),
+    ('참나무통 맑은 이슬6', '하이트 진로', '16.0', '하이트 진로에서 발매한 참나무통 맑은 이슬', 'image.test.url', 3, NOW(), NOW())
